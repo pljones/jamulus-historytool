29419ddb6a23c8a0e66ff21aca9d926fa2e1a068
Peter L Jones <peter@drealm.info>
2022-04-29 19:42:41 +0100

    Add a fixed start date and long term view

--- src/html/js/history_controller.js
+++ src/html/js/history_controller.js
@@ -22,12 +22,23 @@ See LICENCE.txt for the full text.
 function update_graph(url, refresh) {
 
 	if (typeof history_graph !== 'undefined') {
+		$('#rendering_spinner').hide();
+		$('#formatting_spinner').hide();
+		$('#loading_failed').hide();
+		$('#pre_spinner').show();
 		$.get(url, function(data) {
+			$('#pre_spinner').hide();
 			if (typeof data.jamulus_history !== 'undefined') {
-
+				$('#formatting_spinner').show();
 				var svg_graph = history_graph.update(data.jamulus_history);
-				hs_graph.html(svg_graph).show();
+				$('#formatting_spinner').hide();
 
+				$('#rendering_spinner').show();
+				hs_graph.html(svg_graph).show();
+				$('#rendering_spinner').hide();
+
+			} else {
+				$('#loading_failed').show();
 			}
 
 		}, 'json');
@@ -38,24 +49,29 @@ function update_graph(url, refresh) {
 
 $(document).ready(function () {
 	$('.no_script').hide(); // We're javascript, so we can...
+	$('#rendering_spinner').hide();
+	$('#formatting_spinner').hide();
+	$('#loading_failed').hide();
+	$('#pre_spinner').show(); // Loading...
 
 	$.ajaxSetup({
 		cache: false
 	});
 
-	hg_days = 60;
+	hg_days = dayjs().diff(dayjs('2014-11-04', 'YYYY-MM-DD'), 'days');
 	if (typeof HistoryGraph !== 'undefined') {
 		$('#sDays').html(hg_days);
 		history_graph = new HistoryGraph(hg_days);
 	} else {
 		$('#sDays').html('--');
 		history_graph = undefined;
+		$('#loading_failed').show(); // Loading failed
 	}
 
 	hs = $('#history');
 	hs_wait = $('.wait', $('#hHistory'));
 	hs_graph = $('#historyGraph');
 
-	base = location.href.replace(/[^\/]*$/, '').replace(/\/*$/, ''); // anything from the last slash goes
-	update_graph(base + '/php/jamulus_history?days=' + hg_days + '&reverse=false&last=true', 60000);
+	base = location.href.replace(/[^\/]*$/, '').replace(/[\/]*$/, ''); // anything from the last slash goes
+	update_graph(base + '/php/jamulus_history?days=' + hg_days + '&reverse=false&last=true&max_lines=100000', 60000);
 });
